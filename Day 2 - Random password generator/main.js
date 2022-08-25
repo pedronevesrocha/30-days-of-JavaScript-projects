e27ccb5d3132f1b70462364062918ab8d494a44f
Pedro Rocha <pedrofrancisconeves@yahoo.com.br>
2022-08-25 19:22:13 -0300

    add style and bug fix in js

--- Day 2 - Random password generator/main.js
+++ Day 2 - Random password generator/main.js
@@ -4,6 +4,7 @@ function genPassword() {
     var passwordLength = 12
     var password = ''
     for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length)
         password += chars.substring(randomNumber, randomNumber + 1)
     } 
     document.getElementById('password').value = password
