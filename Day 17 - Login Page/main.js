3a1b33930724a98897445f4699dcb4446df7aead
Pedro Rocha <pedrofrancisconeves@yahoo.com.br>
2022-09-15 05:04:32 -0300

    bug fix

--- Day 17 - Login Page/main.js
+++ Day 17 - Login Page/main.js
@@ -1,4 +1,4 @@
-et switchCtn = document.querySelector('#switch-cnt')
+let switchCtn = document.querySelector('#switch-cnt')
 let switchC1 = document.querySelector('#switch-c1')
 let switchC2 = document.querySelector('#switch-c2')
 let switchCircle = document.querySelectorAll('.switch__circle')
