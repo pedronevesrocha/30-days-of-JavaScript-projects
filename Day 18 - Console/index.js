d4c3c6308ad9b4d1ef98838fa04bc0dedd526e34
Pedro Rocha <pedrofrancisconeves@yahoo.com.br>
2022-10-04 11:28:50 -0300

    add index.js

---
+++ Day 18 - Console/index.js
@@ -0,0 +1,11 @@
+// Log an object.
+console.log({a: 'foo', b: 'bar'});
+
+// Log some extra information.
+console.info('Many messages, such logging, wow!');
+
+// Log a warning.
+console.warn('Warning, warning!');
+
+// Log an error.
+console.error('Oh noes, y u do dis?');
